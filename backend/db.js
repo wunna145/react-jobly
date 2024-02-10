d0e11c0b8f6f3ce457162c4e60c114f8639616c5
Zaw Wunna <zawwunna@Zaws-Air.lan>
2024-02-10 01:59:45 -0800

    files added

---
+++ backend/db.js
@@ -0,0 +1,37 @@
+// "use strict";
+// /** Database setup for jobly. */
+// const { Client } = require("pg");
+// const { getDatabaseUri } = require("./config");
+
+// let db;
+
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri()
+//   });
+// }
+
+// db.connect();
+
+// module.exports = db;
+
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+const db = new Client({
+  connectionString: getDatabaseUri(),
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+});
+
+db.connect();
+
+module.exports = db;
